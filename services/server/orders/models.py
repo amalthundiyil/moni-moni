d8a85b9cc88cafc782acf77b492d5ea7b7ac767b
amal-thundiyil <amalthundiyil0@gmail.com>
2022-02-25 16:44:12 +0530

    Working order model

--- services/server/orders/models.py
+++ services/server/orders/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 
-from store.models import Product
+# from store.models import Fundraiser
 
 
 class Order(models.Model):
@@ -9,15 +9,18 @@ class Order(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="order_user"
     )
     full_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
     address1 = models.CharField(max_length=250)
     address2 = models.CharField(max_length=250)
     city = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    post_code = models.CharField(max_length=20)
+    postal_code = models.CharField(max_length=20)
+    country_code = models.CharField(max_length=4, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     total_paid = models.DecimalField(max_digits=5, decimal_places=2)
     order_key = models.CharField(max_length=200)
+    payment_option = models.CharField(max_length=200, blank=True)
     billing_status = models.BooleanField(default=False)
 
     class Meta:
@@ -30,9 +33,9 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
-    product = models.ForeignKey(
-        Product, related_name="order_items", on_delete=models.CASCADE
-    )
+    # fundraiser = models.ForeignKey(
+    #     Fundraiser, related_name="order_items", on_delete=models.CASCADE
+    # )
     price = models.DecimalField(max_digits=5, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
