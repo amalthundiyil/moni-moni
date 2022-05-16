9240b63f0e2b6fbf85524d8a0ed3e768a0a7bbc7
Amal Thundiyil <amalthundiyil0@gmail.com>
2022-05-16 15:15:12 +0530

    ench: home actions

--- moni-moni/client/src/features/home/Home.jsx
+++ moni-moni/client/src/features/home/Home.jsx
@@ -2,7 +2,11 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 import Fundraisers from "../../components/Fundraisers";
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -16,6 +20,36 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function Home() {
   return (
     <>
+      <Container maxWidth="sm">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          Album layout
+        </Typography>
+        <Typography
+          variant="h5"
+          align="center"
+          color="text.secondary"
+          paragraph
+        >
+          Something short and leading about the collection below—its contents,
+          the creator, etc. Make it short and sweet, but not too short so folks
+          don&apos;t simply skip over it entirely.
+        </Typography>
+        <Stack
+          sx={{ pt: 4 }}
+          direction="row"
+          spacing={2}
+          justifyContent="center"
+        >
+          <Button variant="contained">Main call to action</Button>
+          <Button variant="outlined">Secondary action</Button>
+        </Stack>
+      </Container>
       <Box sx={{ flexGrow: 1 }} m={10}>
         <Grid container spacing={2}>
           <Grid item xs={8}>
