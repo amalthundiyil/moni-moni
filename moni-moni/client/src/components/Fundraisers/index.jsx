00921d42d53bcfb91deddc74e6588af51edd9288
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-05-22 21:56:54 +0530

    ench: another carousel
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- moni-moni/client/src/components/Fundraisers/index.jsx
+++ moni-moni/client/src/components/Fundraisers/index.jsx
@@ -1,40 +1,43 @@
-import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import NormalFundraiser from "../Fundraiser/NormalFundraiser";
 import Spinner from "../Spinner";
 import { v4 as uuidv4 } from "uuid";
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
 import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
 
-function Fundraisers({ fundraiser }) {
-  if (!fundraiser) {
-    return <Spinner open={true} />;
-  }
-
+export default function App({ fundraiser }) {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={4}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-      modules={[Autoplay, Pagination, Navigation]}
-    >
-      {fundraiser.map((fundraiser) => (
-        <SwiperSlide>
-          <NormalFundraiser key={uuidv4()} {...fundraiser} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        spaceBetween={30}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+      >
+        {fundraiser.map((fundraiser) => (
+          <SwiperSlide>
+            <NormalFundraiser key={uuidv4()} {...fundraiser} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 }
-
-export default Fundraisers;
