412c740f455b89926df810e4d9a1e6d18eb2e5b6
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-06-05 12:36:53 +0530

    ench: add heroku config varz
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+#!usr/bin/env bash
+
+echo "Deployging to Heroku..."
+
 heroku stack:set container
+heroku config:set $(cat .env | sed '/^$/d; /#[[:print:]]*$/d')
 git push heroku main
