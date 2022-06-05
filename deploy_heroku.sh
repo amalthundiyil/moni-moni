82c29cf2217593a216270e1ca10e35b43c8cbb01
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-06-05 12:27:11 +0530

    heroku
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-heroku container:push --app=moni-moni frontend
-heroku container:release --app=moni-moni frontend
-
-heroku container:release --app=moni-moni backend
-heroku container:release --app=moni-moni backend
+heroku stack:set container
+git push heroku main
