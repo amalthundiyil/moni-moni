b8b55430fe9f313acb0fe2fcc9cb0b991da56bdb
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-10-23 14:07:12 +0530

    add snackbar
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- moni-moni/client/src/features/auth/asyncActions.jsx
+++ moni-moni/client/src/features/auth/asyncActions.jsx
@@ -11,6 +11,7 @@ import {
   verifyUserSuccess,
   userLogout,
 } from "./authSlice";
+import CustomizedSnackbars from "../../components/Snackbar";
 
 const verifyTokenAsync =
   (silentAuth = false) =>
@@ -33,12 +34,18 @@ const userLoginAsync = (email, password) => async (dispatch) => {
   dispatch(userLoginStarted());
 
   const result = await userLoginService(email, password);
-
+  console.log("hello");
+  console.log(result);
   if (result.error) {
     dispatch(userLoginFailure({ error: result.response.data.msg }));
-    return;
+    return (
+      <CustomizedSnackbars message={result.response.data.msg} type={"error"} />
+    );
   }
   dispatch(verifyUserSuccess(result.data));
+  return (
+    <CustomizedSnackbars message={"Logged in successfully"} type={"success"} />
+  );
 };
 
 const userLogoutAsync = () => (dispatch) => {
