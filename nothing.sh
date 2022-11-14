4cc0972556f239e142c95d21171d16e09eb24ffa
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-11-14 20:09:44 +0530

    github actions
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- nothing.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-for file in $(find "./moni-moni/server" -name '*.py' -not -path "./moni-moni/server/venv/*
-");
-do
-  echo $file
-  black $file
-  reorder-python-imports $file
-done
