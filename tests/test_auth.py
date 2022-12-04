39264147c674906d05fc8d2fa30599591b11505c
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-12-04 21:24:07 +0530

    before adding fundraisers test
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,19 +3,6 @@ import json
 import requests
 
 
-# def test_register(base_url, base_headers):
-#     url = base_url + "api/v1/auth/register/"
-#     payload = json.dumps(
-#         {
-#             "user_name": "test",
-#             "email": "test@test.com",
-#             "password": "test",
-#         }
-#     )
-#     response = requests.request("POST", url, headers=base_headers, data=payload)
-#     assert response.ok
-
-
 def test_login(base_url, base_headers):
     url = base_url + "api/v1/auth/login/"
     payload = json.dumps({"email": "test@test.com", "password": "test"})
@@ -35,3 +22,16 @@ def test_logout(base_url, auth_headers, auth_request):
     payload = {}
     response = auth_request.post(url, headers=auth_headers, data=payload)
     assert response.ok
+
+
+# def test_register(base_url, base_headers):
+#     url = base_url + "api/v1/auth/register/"
+#     payload = json.dumps(
+#         {
+#             "user_name": "test",
+#             "email": "test@test.com",
+#             "password": "test",
+#         }
+#     )
+#     response = requests.request("POST", url, headers=base_headers, data=payload)
+#     assert response.ok
