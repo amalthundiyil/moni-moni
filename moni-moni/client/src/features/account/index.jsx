875bc53c725624132385653999ce038b7b1d93e9
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2023-03-03 14:06:30 +0530

    abms here we go again
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- moni-moni/client/src/features/account/index.jsx
+++ moni-moni/client/src/features/account/index.jsx
@@ -65,29 +65,6 @@ export default function Account() {
 
   return (
     <Box sx={{ display: "flex" }}>
-      <Drawer variant="permanent" open={open}>
-        <Toolbar
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-            px: [1],
-          }}
-        >
-          <IconButton onClick={toggleDrawer}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </Toolbar>
-        <Divider />
-        <List component="nav">
-          <ListItemButton onClick={() => setElement("Fundraisers")}>
-            <ListItemIcon>
-              <BarChartIcon />
-            </ListItemIcon>
-            <ListItemText primary="Fundraisers" />
-          </ListItemButton>
-        </List>
-      </Drawer>
       <Box
         component="main"
         sx={{
